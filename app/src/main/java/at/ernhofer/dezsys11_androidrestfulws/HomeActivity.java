ffcd261e7e7851768a733493b35bde30dba4b4b5
Andreas Ernhofer <aernhofer@student.tgm.ac.at>
2016-02-19 21:20:55 +0100

    Created Java Classes

---
+++ app/src/main/java/at/ernhofer/dezsys11_androidrestfulws/HomeActivity.java
@@ -0,0 +1,18 @@
+package at.ernhofer.dezsys11_androidrestfulws;
+
+import android.app.Activity;
+import android.os.Bundle;
+/**
+ *
+ * Home Screen Activity
+ */
+public class HomeActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Displays Home Screen
+        setContentView(R.layout.home);
+    }
+
+}
---
+++ app/src/main/java/at/ernhofer/dezsys11_androidrestfulws/Utility.java
@@ -0,0 +1,36 @@
+package at.ernhofer.dezsys11_androidrestfulws;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+/**
+ * Class which has Utility methods
+ *
+ */
+public class Utility {
+    private static Pattern pattern;
+    private static Matcher matcher;
+    //Email Pattern
+    private static final String EMAIL_PATTERN = "/^(([^<>()\\[\\]\\\\.,;:\\s@\"]+(\\.[^<>()\\[\\]\\\\.,;:\\s@\"]+)*)|(\".+\"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$/";
+
+    /**
+     * Validate Email with regular expression
+     *
+     * @param email
+     * @return true for Valid Email and false for Invalid Email
+     */
+    public static boolean validate(String email) {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+
+    }
+    /**
+     * Checks for Null String object
+     *
+     * @param txt
+     * @return true for not null and false for null String object
+     */
+    public static boolean isNotNull(String txt){
+        return txt!=null && txt.trim().length()>0 ? true: false;
+    }
+}
